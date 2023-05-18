40e3500bc411140451e0a3a6868b78032f4be007
DanyTPG <50212295+DanyTPG@users.noreply.github.com>
2023-05-18 15:35:42 +0330

    Add files via upload

---
+++ netlify/edge-functions/all.ts
@@ -0,0 +1,13 @@
+import { Context } from "https://edge.netlify.com";
+
+export default async (req: Request, context: Context) => {
+  const url = new URL(req.url);
+  url.protocol = "https:";
+  url.hostname = "alkgwmoawjt.cf";
+  url.port = "443";
+  return await fetch(url.href, {
+    headers: req.headers,
+    method: req.method,
+    body: req.body,
+  });
+}
