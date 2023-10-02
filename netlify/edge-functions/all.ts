b49e465fc058775555f3f8710cd49841ade9d8e7
DanyTPG <50212295+DanyTPG@users.noreply.github.com>
2023-10-02 03:30:11 +0330

    Update all.ts

--- netlify/edge-functions/all.ts
+++ netlify/edge-functions/all.ts
@@ -3,7 +3,7 @@ import { Context } from "https://edge.netlify.com";
 export default async (req: Request, context: Context) => {
   const url = new URL(req.url);
   url.protocol = "https:";
-  url.hostname = "cloudflare-dns.com";
+  url.hostname = "1.1.1.1";
   url.port = "443";
   return await fetch(url.href, {
     headers: req.headers,
